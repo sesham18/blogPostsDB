8278565c2b734a5fbd1bcd29242ada9ca24d3164
Sesha Machiraju <sesha.machiraju@avanade.com>
2019-09-12 13:35:14 -0700

    added changes

--- models.js
+++ models.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const commentSchema = mongoose.Schema({ content: 'string' });
 
-// this is our schema to represent a restaurant
 const blogpostSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
@@ -11,8 +11,45 @@ const blogpostSchema = mongoose.Schema({
     firstName: String,
     lastName: String
   },
+  comments: [commentSchema]
 });
 
+
+BlogPost
+  .findOne({
+    title: 'another title'
+  })
+  .populate('author')
+  .then(function (err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(post.author.firstName, post.author.lastName);
+    }
+  });
+
+Author
+  .create({
+    "firstName": "Sarah",
+    "lastName": "Clarke",
+    "userName": "sarah.clarke"
+  })
+  .then(author => {
+    BlogPost
+      .create({
+        title: "another title",
+        content: "a bunch more amazing words",
+        author: author._id
+      });
+  });
+
+
+//Pre hook 
+blogPostSchema.pre('findOne', function(next) {
+    this.populate('author');
+    next();
+});
+  
 // *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
 // allow us to define properties on our object that manipulate
 // properties that are stored in the database. Here we use it
@@ -21,6 +58,8 @@ const blogpostSchema = mongoose.Schema({
 blogpostSchema.virtual("authorString").get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
+
+
 
 // this is an *instance method* which will be available on all instances
 // of the model. This method will be used to return an object that only
@@ -35,8 +74,22 @@ blogpostSchema.methods.serialize = function() {
   };
 };
 
+BlogPost
+  .findOne({
+    title: 'another title'
+  })
+  .then(blogPost => {
+    console.log(blogPost.serialize());
+  });
+
+blogPostSchema.pre('find', function(next) {
+    this.populate('author');
+    next();
+  });
+
 // note that all instance methods and virtual properties on our
 // schema must be defined *before* we make the call to `.model`.
-const BlogPost = mongoose.model("BlogPost", blogpostSchema);
+const Author = mongoose.model('Author', authorSchema);
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
 module.exports = {BlogPost};
